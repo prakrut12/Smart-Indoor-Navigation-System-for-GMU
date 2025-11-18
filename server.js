acea42f4ec3ef1c7029dd2d2eda35408b17a9151
Prakruthi S <168062607+prakrut12@users.noreply.github.com>
2025-11-18 17:53:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const host = '0.0.0.0';
 
 // Middleware
 app.use(cors({
-    origin: "https://smart-indoor-navigation-system-for-gmu.onrender.com",
+    origin: "https://gmu-nav.onrender.com",
     methods: ["GET", "POST", "DELETE"],
     allowedHeaders: ["Content-Type"]
 }));
