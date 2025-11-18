cf510776e3316d217086a7e75836f97ab2f24603
Prakruthi S <168062607+prakrut12@users.noreply.github.com>
2025-11-18 17:24:41 +0530

    Update admin_log.js

--- public/admin_log.js
+++ public/admin_log.js
@@ -1,7 +1,7 @@
 // This function is linked from admin_log.html
 async function fetchLogs() {
     try {
-        const response = await fetch('http://localhost:3000/api/logs');
+        const response = await fetch('https://smart-indoor-navigation-system-for-gmu.onrender.com');
         if (!response.ok) {
             throw new Error('Failed to fetch logs.');
         }
